167db606f1341666f7b2469c970e172f93d13a43
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-16 16:57:18 +0600

    added cart file of html and made it functionally

--- js/cart.js
+++ js/cart.js
@@ -7,6 +7,7 @@ const addProduct = () => {
     quantityField.value = "";
     console.log(product, quantity);
     displayProduct(product, quantity);
+    saveProductToLocalStorage(product, quantity);
 }
 
 const displayProduct = (product, quantity) => {
@@ -15,3 +16,32 @@ const displayProduct = (product, quantity) => {
     li.innerText = `${product}: ${quantity}`;
     ul.appendChild(li);
 }
+
+const getStoredShoppingCart = () => {
+    let cart = {};
+    const storedCart = localStorage.getItem('cart');
+    if (storedCart) {
+        cart = JSON.parse(storedCart);
+    }
+    return cart;
+}
+
+const saveProductToLocalStorage = (product, quantity) => {
+    const cart = getStoredShoppingCart();
+    cart[product] = quantity;
+    const cartStringified = JSON.stringify(cart);
+    // console.log(cartStringified);
+    localStorage.setItem('cart', cartStringified);
+}
+
+const displayProductsFromLocalStorage = () => {
+    const savedCart = getStoredShoppingCart();
+    console.log(savedCart);
+    for (const product in savedCart) {
+        const quantity = savedCart[product];
+        console.log(product, quantity);
+        displayProduct(product, quantity);
+    }
+}
+
+displayProductsFromLocalStorage();
